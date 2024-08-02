ca546eccd3659cd27f40687b9f28e2adf372cf96
tristanm71 <134559647+tristanm71@users.noreply.github.com>
2024-08-02 14:18:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+
+file = input("Enter a file")
+book = open(file, 'rb')
+if not book:
+    exit()
+
 pdfreader = PyPDF2.PdfReader(book)
 pages = len(pdfreader.pages)
 speaker = pyttsx3.init()
-for num in range(7, pages):
+for num in range(pages):
     page = pdfreader.pages[num]
     text = page.extract_text()
     speaker.say(text)
